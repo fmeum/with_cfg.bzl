44c8dbde43d2267063a194fcde5c0133e8cd20f6
Matt Brown <mattbrown@spotify.com>
2024-05-23 22:13:44 -0400

    VersionTest: add check that JVM version is 17 or 21

--- examples/java_test_suite_multiple_jdks/src/test/java/com/example/VersionTest.java
+++ examples/java_test_suite_multiple_jdks/src/test/java/com/example/VersionTest.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class VersionTest {
 
   @Test
   public void test() {
+    // something has gone wrong if the JVM running this test is not 17 or 21, based on the
+    // configuration in BUILD.bazel
+    assertTrue(
+        Set.of("17", "21").contains(System.getProperty("java.specification.version")),
+        "expected test to only be run on JDK version 17 or 21");
+
     List<String> properties = List.of("java.specification.version", "java.vm.vendor", "java.home");
     for (String property : properties) {
       System.out.printf("%s: %s\n", property, System.getProperty(property));
