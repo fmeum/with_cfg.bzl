2abfa8dd1c530f9d1d3ac051e25578a225910da6
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-07-24 12:58:44 +0200

    Reenable test on macOS

--- examples/dynamic_deps/bin_test.sh
+++ examples/dynamic_deps/bin_test.sh
@@ -13,6 +13,11 @@ source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
 # --- end runfiles.bash initialization v3 ---
 
 bin_path=$(rlocation "$1" || { >&2 echo "$1 not found"; exit 1; })
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  otool -l "$bin_path"
+fi
+
 output=$("$bin_path" || { >&2 echo "Failed to run $bin_path"; exit 1; })
 if [[ "$output" != "Hello,_world!" ]]; then
   echo "Unexpected output: $output"
